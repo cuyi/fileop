54240e8a450693cd84a327cd40df856d23447869
cuyi <cuyi@qq.com>
2024-07-14 22:16:03 +0800

    second commit

---
+++ build/src/CMakeFiles/fileop.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/toyi/code/fo/src/common.c" "src/CMakeFiles/fileop.dir/common.c.o" "gcc" "src/CMakeFiles/fileop.dir/common.c.o.d"
+  "/home/toyi/code/fo/src/main.c" "src/CMakeFiles/fileop.dir/main.c.o" "gcc" "src/CMakeFiles/fileop.dir/main.c.o.d"
+  "/home/toyi/code/fo/src/read/file_read.c" "src/CMakeFiles/fileop.dir/read/file_read.c.o" "gcc" "src/CMakeFiles/fileop.dir/read/file_read.c.o.d"
+  "/home/toyi/code/fo/src/write/file_write.c" "src/CMakeFiles/fileop.dir/write/file_write.c.o" "gcc" "src/CMakeFiles/fileop.dir/write/file_write.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/fileop.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "/home/toyi/code/fo/bin/fileop"
+  "/home/toyi/code/fo/bin/fileop.pdb"
+  "CMakeFiles/fileop.dir/common.c.o"
+  "CMakeFiles/fileop.dir/common.c.o.d"
+  "CMakeFiles/fileop.dir/main.c.o"
+  "CMakeFiles/fileop.dir/main.c.o.d"
+  "CMakeFiles/fileop.dir/read/file_read.c.o"
+  "CMakeFiles/fileop.dir/read/file_read.c.o.d"
+  "CMakeFiles/fileop.dir/write/file_write.c.o"
+  "CMakeFiles/fileop.dir/write/file_write.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/fileop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
